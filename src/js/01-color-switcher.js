a6d13ca5f9d5c227bd06b47193365ec39ed88414
SergeZed <75592154+SergeZed@users.noreply.github.com>
2022-06-21 18:47:17 +0700

    Tasks 1 & 2 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const logger = () =>
+  (document.body.style.backgroundColor = getRandomHexColor());
+
+let intervalId = '';
+
+stopBtn.disabled = true;
+
+function startChangingColors(e) {
+  intervalId = setInterval(logger, 2000, 2000);
+  stopBtn.disabled = false;
+  startBtn.disabled = true;
+}
+
+function stopChangingColors(e) {
+  clearInterval(intervalId);
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+}
+
+startBtn.addEventListener('click', startChangingColors);
+stopBtn.addEventListener('click', stopChangingColors);
---
+++ src/js/02-timer.js
@@ -0,0 +1,66 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+const inputEl = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+let myTimer = 'null';
+let d;
+let CURRENT_DATE = new Date();
+const daysMy = document.querySelector('[data-days]');
+const hoursMy = document.querySelector('[data-hours]');
+const minutesMy = document.querySelector('[data-minutes]');
+const secondsMy = document.querySelector('[data-seconds]');
+let myDelta;
+startBtn.addEventListener('click', startTimer);
+startBtn.disabled = true;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    if (selectedDates[0] < CURRENT_DATE) {
+      window.alert('Please choose a date in the future');
+    } else {
+      startBtn.disabled = false;
+      startBtn.addEventListener('click', timer(selectedDates[0]));
+    }
+  },
+};
+flatpickr('#datetime-picker', options);
+function startTimer(f) {
+  myTimer = setInterval(() => {
+    const delta = getMyDelta();
+    renderTimer(delta);
+  }, 1000);
+}
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
+const timer = targetDate => {
+  return function () {
+    setInterval(() => {
+      const delta = new Date(targetDate) - new Date();
+      renderTimer(delta);
+    }, 1000);
+  };
+};
+const renderTimer = string => {
+  let { days, hours, minutes, seconds } = convertMs(string);
+  daysMy.textContent = days;
+  minutesMy.textContent = minutes;
+  hoursMy.textContent = hours;
+  secondsMy.textContent = seconds;
+};
+function getMyDelta(now, future) {
+  myDelta = d - CURRENT_DATE;
+  return myDelta;
+}
